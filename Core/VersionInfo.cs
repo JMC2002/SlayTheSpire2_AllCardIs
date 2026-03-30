6498eb2371cbea2c2864d2c8a84654ee433132d2
MC J <jmc8@qq.com>
2026-03-31 03:14:28 +0800

    更新版本号

--- Core/VersionInfo.cs
+++ Core/VersionInfo.cs
@@ -3,7 +3,7 @@
 public static class VersionInfo
 {
     public const string Name = "AllCardIs";
-    public const string Version = "1.1.0";
+    public const string Version = "1.2.0";
 
     public static string Tag => $"[{Name} v{Version}]";
 }
